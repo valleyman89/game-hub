9869a05f1cc6341a380500b2a878941c5d3c08f3
valleyman89 <dev@stevenrolph.com>
2023-08-04 19:54:41 +0100

    filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -31,8 +31,8 @@ const PlatformIconList = ({ platforms }: Props) => {
   };
   return (
     <HStack marginY={1}>
-      {platforms.map((platform, index) => (
-        <Icon key={index} as={iconMap[platform.slug]} color="gray.500" />
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );

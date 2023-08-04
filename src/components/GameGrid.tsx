fd4b37bb2758b548547d73111782c6868d906632
valleyman89 <dev@stevenrolph.com>
2023-08-04 20:22:49 +0100

    build platform select

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -26,11 +26,13 @@ const GameGrid = ({ selectedGenre }: Props) => {
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
+        {!isLoading
+          ? data.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))
+          : null}
       </SimpleGrid>
     </>
   );
